5c41fee772bfbeadc220698ab61e33cc9ce8e6aa
Alireza <alireza27163@gmail.com>
2021-12-19 18:18:46 +0200

    Fixed bugs

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,8 +6,8 @@ export const baseUrl = "https://bayut.p.rapidapi.com"
 export const fetchApi = async (url) => {
     const {data} = await axios.get((url), {
         headers: {
-            'x-rapidapi-host': process.env.REACT_APP_HOST,
-            'x-rapidapi-key': process.env.REACT_APP_KEY
+            'x-rapidapi-host': 'bayut.p.rapidapi.com',
+            'x-rapidapi-key': '1d1404a2efmsh16a3c870f1b451bp180367jsn53827b573567'
           }
     })
     return data;
